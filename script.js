50ef6acb79eea4c7884e4a9f61a3d3f0ea7e4656
ShivasaiCEng <isha4shiva@gmail.com>
2026-03-08 16:21:28 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,156 @@
+
+let villagepart = document.getElementById("village");
+let mainstorypart=document.getElementById("mainstory");
+let Foreststorypart=document.getElementById("forest");
+let dragonstorypart=document.getElementById("dragon");
+let playername="";
+const NxtBtn=document.getElementById("nextbtn");
+NxtBtn.style.display="none";
+
+let villagebtn=document.getElementById("Gotovillage");
+villagebtn.addEventListener("click",function(){
+mainstorypart.style.display="none";
+villagepart.style.display="block";
+Foreststorypart.style.display="none";
+dragonstorypart.style.display="none";
+NxtBtn.style.display="none";
+
+});
+
+
+let Forestbtn=document.getElementById("enterforest");
+Forestbtn.addEventListener("click", function(){
+mainstorypart.style.display="none";
+villagepart.style.display="none";
+Foreststorypart.style.display="block";
+dragonstorypart.style.display="none";
+NxtBtn.style.display="block";
+
+});
+
+let Dragonbtn=document.getElementById("fightdragon");
+Dragonbtn.addEventListener("click",function(){
+mainstorypart.style.display="none";
+villagepart.style.display="none";
+Foreststorypart.style.display="none";
+dragonstorypart.style.display="block";
+NxtBtn.style.display="block";
+
+})
+
+let submitbtn=document.getElementById("submitName");
+
+    let greeting=document.getElementById("merchantGreeting");  // <p> <p>
+submitbtn.addEventListener("click",function(){
+    let name=document.getElementById("playername").value;
+    playername=name;
+  greeting.innerText="Merchant: Nice to meet you," + name + " welcome";
+    NxtBtn.style.display="block";
+})
+
+//merchant talk part
+// let Merchanttalkbtn=document.getElementById("talktomerchant");
+  let merchantd1=document.getElementById("merchantd1");
+//  merchantd1.style.display="none";
+// Merchanttalkbtn.addEventListener("click",function(){
+// merchantd1.style.display="block";
+// })
+
+// let merchantd2=document.getElementById("merchantd2");
+// merchantd2.style.display="none";
+// let nextbtn=document.getElementById("nextbtn");
+
+// let merchantd3=document.getElementById("merchantd3");
+// merchantd3.style.display="none";
+// nextbtn.addEventListener("click", function(){
+//  merchantd2.style.display="block";
+// })
+
+const TalkToMerchant=document.getElementById("talktomerchant");
+
+const dialogueBox=document.getElementById("merchantDialogue");
+const MerchantTaskDecision=document.getElementById("MerchantTaskDecision");
+let merchanttaskaccept=document.getElementById("merchanttaskaccept");
+let merchanttaskreject=document.getElementById("merchanttaskreject");
+
+TalkToMerchant.addEventListener("click",()=>{
+merchantd1.style.display="block";
+NxtBtn.style.display="block"; 
+})
+
+
+ let index=0;
+
+NxtBtn.addEventListener("click", ()=>{
+ const dialogues=[
+  "Merchant: What do you want",
+  `${playername}:supplies!`,  //playername.value is invalid as it is not an input element it is a string so it read it as undefined.
+  "Merchant: 100XP or Bring me few natural extracts from the forest"
+]
+
+  if(index<dialogues.length){
+    dialogueBox.innerText=dialogues[index];
+    index++;
+  }
+
+  if(index===dialogues.length){
+    MerchantTaskDecision.style.display="block";
+     NxtBtn.style.display="none";
+  }
+});
+
+merchanttaskaccept.addEventListener("click",()=>{
+  dialogueBox.innerText="Merchant:Okay good! remember the forest is too dark";
+  villagepart.style.display="none";
+  Foreststorypart.style.display="block";
+})
+let TalkToWizard=document.getElementById("Talktowizard");
+let Wizdialoguebox=document.getElementById("WizDialogueBox");
+let ForestDialogues=[];
+
+let i=0;
+TalkToWizard.addEventListener("click", ()=>{
+  
+  
+  ForestDialogues=[
+  "Wiz:Hey what do you want?",
+  `${playername}: I am searching for natural extracts for the merchant in the village`,
+  "wiz:Don't pass thhrough this forest many have lost their lives for that precious natural extract",
+  `${playername}: I need to save the village from dragon`,
+  "wiz: Are you the warrior from the cathedral of the great Shindro Ochinawa",
+  `${playername}: Yes but how do you know?`,
+"wiz: Please forgive me take this gem it will guide you to the place you are going.",
+"You got the natural extracts sell them and get your sword!"
+];
+i=0;
+Wizdialoguebox.innerText = ForestDialogues[i];
+NxtBtn.style.display="block";
+})
+
+
+NxtBtn.addEventListener("click", ()=>{
+  if(ForestDialogues.length === 0) return; //the wizard dialogue stops if there are no other dialogues in the wizarddialoguebox which will then help us in implementing both merchant and wizard dialogue separately.
+  NxtBtn.style.display="block";
+  if(i < ForestDialogues.length - 1){
+    i++;
+    Wizdialoguebox.innerText = ForestDialogues[i];
+}
+
+else{
+ Foreststorypart.style.display="none";
+  villagepart.style.display="block";
+i=0;
+index=0;
+    //this was done to reset the prev dialogues and  make a new convo session.
+    ForestDialogues=[];
+    Wizdialoguebox.innerText="";
+dialogueBox.innerText="";
+MerchantTaskDecision.style.display="none";
+NxtBtn.style.display="none";
+
+}
+});
+
+merchanttaskreject.addEventListener("click",()=>{
+  dialogueBox.innerText="Guide:It's better you help him.";
+})
